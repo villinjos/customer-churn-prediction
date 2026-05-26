f759b4f251e3de09b08cf1e1e7fa6ebad9b35165
villinjos <josephorribhabor@gmail.com>
2026-05-26 21:26:16 +0100

    Update churn_model.py

--- data/src/churn_model.py
+++ data/src/churn_model.py
@@ -1,1 +1,13 @@
-print("Customer Churn Prediction Project")
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv("data/customer_churn.csv")
+
+# Display first 5 rows
+print(df.head())
+
+# Dataset information
+print(df.info())
+
+# Missing values
+print(df.isnull().sum())
