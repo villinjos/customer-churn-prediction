5d7bd2aa6ef3ef1bf1d1ecfb5659df10e21436e7
villinjos <josephorribhabor@gmail.com>
2026-05-26 21:49:48 +0100

    Update churn_model.py

--- data/src/churn_model.py
+++ data/src/churn_model.py
@@ -1,13 +1,20 @@
 import pandas as pd
 
 # Load dataset
-df = pd.read_csv("data/customer_churn.csv")
+df = pd.read_csv("data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
 # Display first 5 rows
+print("First 5 rows:")
 print(df.head())
 
-# Dataset information
+# Dataset shape
+print("\nDataset Shape:")
+print(df.shape)
+
+# Column information
+print("\nDataset Info:")
 print(df.info())
 
 # Missing values
+print("\nMissing Values:")
 print(df.isnull().sum())
